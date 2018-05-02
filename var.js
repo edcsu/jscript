cf74b741fc51c1278720255fce94fe65b1992ea0
Ssewannonda Keith <skeith696@gmail.com>
2018-05-02 15:23:00 +0300

    js new

---
+++ var.js
@@ -0,0 +1,31 @@
+/*var average;
+average = 2;
+// var average =2;
+console.log(average);*/
+
+var x ;
+var y ;
+var z ;
+
+/*
+x = 1;
+y = 3;
+z = 4;
+
+
+if (x > 10)
+    {
+        console.log(x);
+    }
+
+console.log(y);
+console.log(z);
+*/
+
+x = 5;
+y = '5';
+
+if (x == y) 
+{
+    console.log("They are equal");
+}
